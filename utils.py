694abf57508287a26823631f28e73248c66a4b59
AK <kutkin@gmail.com>
2024-01-17 14:38:20 +0000

    utils

---
+++ utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 16:15:45 2023
+
+@author: kutkin
+"""
+
+import sys
+import casacore.tables as ct
+
+# msin = '/home/kutkin/mnt/kutkin/lockman/test/200108171_06.MS'
+
+msin = sys.argv[1]
+
+nchans = ct.table(msin).getcol('DATA').shape[1]
+
+print(nchans)
